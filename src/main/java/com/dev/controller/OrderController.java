71c03ec5cfa6e4b824ffed7a172c8c44bcbbb522
Awasthi <kawasthi@deloitte.com>
2024-04-23 00:20:08 +0530

    added correlationId tracking

--- src/main/java/com/dev/controller/OrderController.java
+++ src/main/java/com/dev/controller/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.logging.Logger;
 
 @CrossOrigin("http://localhost:8081")
@@ -18,27 +19,26 @@ public class OrderController {
 
     Logger logger = Logger.getLogger(OrderController.class.getName());
     @PostMapping("/customer")
-    public ResponseEntity<String> placeSalesOrder(@Valid @RequestBody Salesorder salesorder) {
-        logger.info("Receieved salesorder message");
+    public ResponseEntity<String> placeSalesOrder(@Valid @RequestBody Salesorder salesorder, @RequestHeader Map<String,String> headers) {
+        logger.info("Receieved salesorder message, correlationId: "+ headers.get("x-correlation-id"));
         return new ResponseEntity<>("salesorder extended to customer service successfully", HttpStatus.CREATED);
     }
 
     @PostMapping("/item-ext")
-    public ResponseEntity<String> placeReturnOrder(@Valid @RequestBody Returnorder returnorder) {
-        logger.info("Receieved returnorder message");
+    public ResponseEntity<String> placeReturnOrder(@Valid @RequestBody Returnorder returnorder,@RequestHeader Map<String,String> headers) {
+        logger.info("Receieved returnorder message, correlationId: " + headers.get("x-correlation-id"));
         return new ResponseEntity<>("returnorder initiated to downstream systems successfully", HttpStatus.CREATED);
     }
 
     @PostMapping("/product")
-    public ResponseEntity<String> placePurchaseOrderToProductService(@Valid @RequestBody Purchaseorder purchaseorder) {
-        logger.info("Receieved purchaseorder message for product inventory update");
+    public ResponseEntity<String> placePurchaseOrderToProductService(@Valid @RequestBody Purchaseorder purchaseorder,@RequestHeader Map<String,String> headers) {
+        logger.info("Receieved purchaseorder message for product inventory update,correlationId: " + headers.get("x-correlation-id"));
         return new ResponseEntity<>("purchaseorder sent to product inventory successfully", HttpStatus.CREATED);
     }
 
     @PostMapping("/shipping")
-    public ResponseEntity<String> placePurchaseOrderToShippingService(@Valid @RequestBody Purchaseorder purchaseorder) {
-        logger.info("Receieved purchaseorder message for shipping");
+    public ResponseEntity<String> placePurchaseOrderToShippingService(@Valid @RequestBody Purchaseorder purchaseorder,@RequestHeader Map<String,String> headers) {
+        logger.info("Receieved purchaseorder message for shipping,correaltionId: " + headers.get("x-correlation-id"));
         return new ResponseEntity<>("purchaseorder sent to shipping service successfully", HttpStatus.CREATED);
     }
-
 }
